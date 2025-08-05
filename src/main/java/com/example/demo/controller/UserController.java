2667302348ff4d0eb0a68eb0fc843bd1643964a2
Jadenmeister <bigdragon00@naver.com>
2025-08-05 15:12:32 +0900

    fix: controller에서 비교검증 시행, (이벤트(오류메세지)등은 해당 페이지에서 작동할 수 있도록 하기)

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Controller
 public class UserController {
 
-    private List<User> userList = new ArrayList<>();
+    private List<User> userDataList = new ArrayList<>();
 
     // 사용자 등록 폼을 보여주는 메소드
     // @GetMapping: HTTP GET 요청을 처리하는 메소드
@@ -36,10 +36,23 @@ public class UserController {
         // user 객체는 @ModelAttribute에 의해 자동으로 생성됨
         // user 객체의 필드에 폼 데이터가 바인딩됨
 
-
+       for(User duplicateUser : userDataList) {
+           if(duplicateUser.getUsername().equals(user.getUsername())) {
+               // 중복이 되는 경우
+                model.addAttribute("error", "이미 사용중인 아이디 입니다. ");
+                user.setUsername("");// 중복된 아이디를 빈 문자열로 초기화
+                model.addAttribute("user", user);
+                return "signup"; // 다시 회원가입 폼으로 돌아감
+           }
+       }
+        // 검증이 끝나면
         // 모델에 사용자 정보를 추가
-        model.addAttribute("user", user);
+        userDataList.add(user);
 
+        model.addAttribute("user", user);
+        model.addAttribute("userDataList", userDataList);
+        model.addAttribute("success", "회원가입이 완료되었습니다!");
+        // 결과페이지를 반환함
         return "result";
     }
 }
